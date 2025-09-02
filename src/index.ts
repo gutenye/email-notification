59c82ed0de17f68499bff34f218ede6811ddbdc7
Guten Ye <ywzhaifei@gmail.com>
2025-09-02 14:54:55 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,33 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { EmailMessage } from 'cloudflare:email'
+import { createMimeMessage } from 'mimetext'
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+		const senderName = 'Hello'
+		const senderAddress = env.SENDER
+		const recipientAddress = env.RECIPIENT
+		const subject = 'An email generated in a worker'
+		const message = `Congratulations, you just sent an email from a worker.`
+
+		const msg = createMimeMessage()
+		msg.setSender({ name: senderName, addr: senderAddress })
+		msg.setRecipient(recipientAddress)
+		msg.setSubject(subject)
+		msg.addMessage({
+			contentType: 'text/plain',
+			data: message,
+		})
+
+		const emailMessage = new EmailMessage(senderAddress, recipientAddress, msg.asRaw())
+		try {
+			await env.sendEmail.send(emailMessage)
+		} catch (error) {
+			if (error instanceof Error) {
+				return new Response(error.message)
+			}
+			return new Response('An unknown error occurred')
+		}
+
+		return new Response('Hello Send Email World!')
 	},
-} satisfies ExportedHandler<Env>;
+} satisfies ExportedHandler<Env>
