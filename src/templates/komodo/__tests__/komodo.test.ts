bb5d786411531e4b663c201621f960d858a9f791
Guten Ye <ywzhaifei@gmail.com>
2025-09-08 06:22:52 +0800

    test dir

--- src/templates/komodo/__tests__/komodo.test.ts
+++ src/templates/komodo/__tests__/komodo.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from 'vitest'
-import { komodo } from '../komodo/komodo'
+import { komodo } from '../komodo'
 
 const serverName = 'Server1'
 const komodoHost = 'https://komodo.com'
